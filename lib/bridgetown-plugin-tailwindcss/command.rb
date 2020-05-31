36d2b8d25776e0d4535c310a9cc0e539060eeb78
paramagicdev <konnor5456@gmail.com>
2020-05-31 19:27:34 -0400

    fixing webpack

--- lib/bridgetown-plugin-tailwindcss/command.rb
+++ lib/bridgetown-plugin-tailwindcss/command.rb
@@ -4,101 +4,121 @@ require "bridgetown"
 require_relative "utils"
 
 class TailwindInit < Bridgetown::Command
-  ACTIONS = TailwindCss::Utils::Actions.new
+ACTIONS = TailwindCss::Utils::Actions.new
 
-  class << self
-    def init_with_program(prog)
-      prog.command(:tailwind_init) do |c|
-        c.syntax "tailwind_init"
-        c.description "Initialize config for Tailwind"
-        # c.option 'path', '--path', 'Choose the path to install tailwind'
+class << self
+  def init_with_program(prog)
+    prog.command(:tailwind_init) do |c|
+      c.syntax "tailwind_init"
+      c.description "Initialize config for Tailwind"
+      # c.option 'path', '--path', 'Choose the path to install tailwind'
 
-        c.action do |_args, _options|
-          Bridgetown::Commands::TailwindInit.new.run
-        end
+      c.action do |_args, _options|
+        run
       end
     end
+  end
 
-    private
+  private
 
-    def run
-      write_files
+  def run
+    write_files
+  end
+
+  def write_files
+    webpack_config = File.expand_path("webpack.config.js")
+    tailwind_config = File.expand_path("tailwind.config.js")
+
+
+    ACTIONS.inject_into_file(webpack_config, webpack_file_contents,
+                             after: base_webpack_loader_string) do
+      webpack_file_contents
     end
 
-    def write_files
-      webpack_config = File.expand_path("webpack.config.js")
-      tailwind_config = File.expand_path("tailwind.config.js")
+    ACTIONS.create_file(tailwind_config, tailwind_config_contents)
+    prepend_to_stylesheet
+  end
 
-      ACTIONS.create_file(webpack_config, webpack_file_contents)
-      ACTIONS.create_file(tailwind_config, tailwind_config_contents)
-      prepend_to_stylesheet
-    end
+  def webpack_injection_site
+    '{
+      loader: "sass-loader",
+      options: {
+        sassOptions: {
+          includePaths: [
+            path.resolve(__dirname, "src/_components"),
+            path.resolve(__dirname, "src/_includes"),
+          ],
+        },
+      },
+    },'
+  end
 
-    def prepend_to_stylesheet
-      frontend_stylesheet = File.join("frontend", "styles", "index.scss")
-      frontend_stylesheet = File.expand_path(frontend_stylesheet)
+  def prepend_to_stylesheet
+    frontend_stylesheet = File.join("frontend", "styles", "index.scss")
+    frontend_stylesheet = File.expand_path(frontend_stylesheet)
 
-      return unless File.exist?(frontend_stylesheet)
+    return unless File.exist?(frontend_stylesheet)
 
-      ACTIONS.prepend_to_file(frontend_stylesheet, import_tailwind_contents)
-    end
+    ACTIONS.prepend_to_file(frontend_stylesheet, import_tailwind_contents)
+  end
 
-    def import_tailwind_contents
-      <<~IMPORT
-        @import 'tailwindcss/base';
-        @import 'tailwindcss/components';
-        @import 'tailwindcss/utilities';
-      IMPORT
-    end
+  def import_tailwind_contents
+    <<~IMPORT
+      @import 'tailwindcss/base';
+      @import 'tailwindcss/components';
+      @import 'tailwindcss/utilities';
 
-    def tailwind_config_contents
-      <<~TAILWIND
-        module.exports = {
-          purge: {
-            enabled: true,
-            content: ['./src/**/*.html'],
-          },
-          theme: {
-            extend: {},
-          },
-          variants: {},
-          plugins: [],
-        }
-      TAILWIND
-    end
+    IMPORT
+  end
 
-    def webpack_file_contents
-      <<~WEBPACK
-        {
-          test: /\.(s[ac]|c)ss$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            "css-loader",
-            {
-              loader: "sass-loader",
-              options: {
-                sassOptions: {
-                  includePaths: [
-                    path.resolve(__dirname, "src/_components"),
-                    path.resolve(__dirname, "src/_includes"),
-                  ],
-                },
-              },
-            },
-            {
-              loader: "postcss-loader",
-              options: {
-                ident: "postcss",
-                plugins: [
-                  require("postcss-import"),
-                  require("tailwindcss"),
-                  require("autoprefixer"),
+  def tailwind_config_contents
+    <<~TAILWIND
+      module.exports = {
+        purge: {
+          enabled: true,
+          content: ['./src/**/*.html'],
+        },
+        theme: {
+          extend: {},
+        },
+        variants: {},
+        plugins: [],
+      }
+    TAILWIND
+  end
+
+  def webpack_file_contents
+    <<~WEBPACK
+      {
+        test: /\.(s[ac]|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sassOptions: {
+                includePaths: [
+                  path.resolve(__dirname, "src/_components"),
+                  path.resolve(__dirname, "src/_includes"),
                 ],
               },
             },
-          ],
-        },
-      WEBPACK
-    end
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: [
+                require("postcss-import"),
+                require("tailwindcss"),
+                require("autoprefixer"),
+              ],
+            },
+          },
+        ],
+      },
+    WEBPACK
   end
+end
 end
