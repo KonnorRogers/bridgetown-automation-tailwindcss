8193915cbac1b0ad51815ae97592cfd1c036517d
ParamagicDev <konnor5456@gmail.com>
2020-06-03 15:33:52 -0400

    ripping out testing ssuite

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -1,11 +1,15 @@
 require "test_helper"
 require "fileutils"
 
-
-module TailwindCss
-  class IntegrationTest < Minitest::Test
-    def test_it_works
-      assert true
-    end
+class IntegrationTest < Minitest::Test
+  def test_it_works
+    Rake.rm_rf(TEST_APP)
+    Rake.mkdir_p(TEST_APP)
+    Rake.cd(TEST_APP)
+    Rake.sh("gem install bundler")
+    Rake.sh("bundle init")
+    Rake.sh("bundle add bridgetown -v '0.15.0.beta-2'")
+    Rake.sh("bundle install")
+    Rake.sh("bundle exec bridgetown new . --force")
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,60 +1,10 @@
 # frozen_string_literal: true
 
 require "minitest"
-require "rake"
 require "minitest/autorun"
-require "bridgetown"
-require "thor"
-require "bridgetown-plugin-tailwindcss"
-
-Bridgetown.logger.log_level = :error
-
-THOR = TailwindCss::Utils::Action.new
-
-PLUGIN_NAME = "bridgetown-plugin-tailwindcss"
+require "rake"
 
 ROOT_DIR = File.expand_path("..", __dir__)
-GEMSPEC_FILE = File.join(ROOT_DIR, "#{PLUGIN_NAME}.gemspec")
-
-NPM_TARBALL = File.join(ROOT_DIR, "#{PLUGIN_NAME}-v#{TailwindCss::VERSION}.tgz")
 
 TEST_DIR = File.expand_path(__dir__)
-
 TEST_APP = File.expand_path("test_app")
-TEST_APP_GEMFILE = File.join(TEST_APP, "Gemfile")
-
-def install_packages
-  Rake.sh("rake install:local")
-
-  # https://medium.com/@the1mills/how-to-test-your-npm-module-without-publishing-it-every-5-minutes-1c4cb4b369be
-  Rake.sh("yarn pack") # Packs into a .tgz file similar to npm
-end
-
-def create_bridgetown_app
-  current_dir = Dir.pwd
-
-  install_packages
-
-  Rake.rm_rf(TEST_APP)
-  Rake.mkdir_p(TEST_APP)
-  Rake.cd(TEST_APP)
-  Rake.sh("gem install bundler")
-  Rake.sh("bundle init --gemspec #{GEMSPEC_FILE}")
-  Rake.sh("bundle install")
-  Rake.sh("bundle exec bridgetown new . --force")
-
-  THOR.append_to_file(TEST_APP_GEMFILE) do
-    <<~GEMFILE
-      group :bridgetown_plugins do
-        gem 'bridgetown-plugin-tailwindcss', '#{TailwindCss::VERSION}'
-      end
-    GEMFILE
-  end
-
-  Rake.sh("yarn add #{NPM_TARBALL}")
-  Rake.sh("bundle install")
-  Rake.sh("bridgetown tailwind_init")
-  Rake.cd(current_dir)
-end
-
-create_bridgetown_app
