258bda691e2d39cd79d4ae3e939c872f6519bdc2
ParamagicDev <konnor5456@gmail.com>
2020-06-04 01:52:43 -0400

    added tests'

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -4,6 +4,13 @@ require 'bundler'
 class IntegrationTest < Minitest::Test
   include TailwindCss::IoTestHelpers
 
+  def run_setup
+    Rake.rm_rf(TEST_APP)
+    Rake.mkdir_p(TEST_APP)
+    Rake.cd(TEST_APP)
+    Rake.sh("bundle install")
+  end
+
   def read_test_file(filename)
     File.read(File.join(TEST_APP, filename))
   end
@@ -12,18 +19,11 @@ class IntegrationTest < Minitest::Test
     File.read(File.join(TEMPLATES_DIR, filename))
   end
 
-  def test_it_works
-    Rake.rm_rf(TEST_APP)
-    Rake.mkdir_p(TEST_APP)
-    Rake.cd(TEST_APP)
+  def current_commit_hash
+    %x(git rev-parse HEAD)
+  end
 
-    Rake.sh("bundle install")
-
-    # This has to overwrite `webpack.config.js`
-    simulate_stdin("y") do
-      Rake.sh("bundle exec bridgetown new . --force --apply='../bridgetown.automation.rb'")
-    end
-
+  def run_assertions
     tailwind = "tailwind.config.js"
     test_tailwind_file = read_test_file(tailwind)
     template_tailwind_file = read_template_file(tailwind)
@@ -43,5 +43,33 @@ class IntegrationTest < Minitest::Test
     template_styles_file = read_template_file(styles)
 
     assert test_styles_file.include?(template_styles_file)
+  end
+
+  def test_it_works_with_local_automation
+    run_setup
+
+    # This has to overwrite `webpack.config.js` so it needs input
+    simulate_stdin("y") do
+      Rake.sh("bundle exec bridgetown new . --force --apply='../bridgetown.automation.rb'")
+    end
+
+    run_assertions
+  end
+
+  def test_it_works_with_remote_automation
+    Rake.sh("bundle exec bridgetown new . --force")
+
+    github_url = "raw.githubusercontent.com"
+    user_and_reponame = "ParamagicDev/bridgetown-plugin-tailwindcss"
+
+    file = "bridgetown.automation.rb"
+
+    url = "#{github_url}/#{user_and_reponame}/#{current_commit_hash}/#{file}"
+
+    simulate_stdin("y") do
+      Rake.sh("bundle exec bridgetown apply #{url}")
+    end
+
+    run_assertions
   end
 end
