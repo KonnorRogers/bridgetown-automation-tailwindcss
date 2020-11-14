b4b0e61e44b8fa1994ecfe9dd9603eb393f09fbf
paramagicdev <konnor5456@gmail.com>
2020-11-14 08:24:51 -0500

    fix: minor error in naming

--- bridgetown.automation.rb
+++ bridgetown.automation.rb
@@ -71,7 +71,7 @@ def add_config_files
     say "Creating #{filename}", :green
 
     force = (ENV['TAILWIND_INTEGRATION_TEST'] == 'true')
-    create_file(filename, File.read(webpack_config), force: force)
+    create_file(filename, File.read(filename), force: force)
   end
 end
 
