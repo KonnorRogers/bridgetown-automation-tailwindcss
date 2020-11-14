1807f8b8fe614e4f43eab3a7989d573a29775264
paramagicdev <konnor5456@gmail.com>
2020-11-14 07:50:16 -0500

    feat: add postcss config

---
+++ postcss.config.js
@@ -0,0 +1,22 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: [
+    "./output/**/*.html",
+    "./frontend/**/*.js",
+    "./src/**/*.md",
+    "./src/**/*.html",
+    "./src/**/*.liquid",
+  ],
+  defaultExtractor: (content) => {
+    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+    return broadMatches.concat(innerMatches);
+  },
+});
+
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
+};
