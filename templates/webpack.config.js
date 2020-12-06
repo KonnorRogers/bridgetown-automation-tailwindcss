e58acbb231627ee36f6461267ddf0edfb2460b7b
Konnor Rogers <konnor5456@gmail.com>
2020-12-06 16:51:00 -0500

    Fix: missing comma in Webpack config

--- templates/webpack.config.js
+++ templates/webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
           "css-loader",
           {
             loader: "postcss-loader",
-          }
+          },
           {
             loader: "sass-loader",
             options: {
